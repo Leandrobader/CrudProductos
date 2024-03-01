1c7f7a6e8176a3457e8cdbaab62f7e7d1fe9fa5e
Leanbader <baderleandro@gmail.com>
2024-03-01 02:43:21 -0300

    se desarrollo la lectura de los productos desde el servidor

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,12 +1,49 @@
-
-
+import { useEffect, useState } from "react";
+import { Container, Row} from "react-bootstrap";
+import axios from "axios";
+import CardProducto from "../sections/CardProducto"
 const Home = () => {
+    //DECLARAMOS EL USESTATE PARA GUARDAR LOS OBJETOS QUE ME TRAE LA RESPUESTA
+    const [productos, setProductos]=useState([]);
+    useState
+    const API = import.meta.env.VITE_API;
+    const getProductos=async()=>{
+        try {
+            const response = await axios.get(`${API}/productos`);
+            console.log("RESPONSE AXIOS --> ", response);
+            setProductos(response.data)//AQUI YA NOS DA EL OBJETO DESESTRUCTURADO
+        } catch (error) {
+            console.log("ERROR-->", error);
+        }
+    } 
+    //AHORA LLAMAREMOS A LA FUNCION GET PRODUCTOS CON LOS METODOS Y EFECTOS USEEFFECT
 
-    return (
-        <div>
-           <h1>Desde Home</h1> 
-        </div>
-    );
+    useEffect(()=>{
+        getProductos();
+        return()=>{//CON ESE RETURN HACEMOS EL DESMONTAJE PARA QUE NO CONSUMA MEMORIA RAM
+            setProductos([]);
+        }
+    },[])//el array vacio significa que la funcion se ejecutara solamente en el montaje
+
+
+  return (
+    <div>
+      <div className="text-center">
+        <h2>Catálogo de Productos</h2>
+      </div>
+      <div className="my-5">
+        <Container>
+          <Row>
+            {productos.map((element, index)=>{
+                return(
+                    <CardProducto producto={element} key={index}></CardProducto>
+                )
+            })}
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Components/sections/ListadoProductos.jsx
+++ src/Components/sections/ListadoProductos.jsx
@@ -1,10 +1,60 @@
+import { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
+import Producto from "./Producto";
 
 const ListadoProductos = () => {
-    return (
-        <div>
-            <h1>Listado de Productos</h1>
+    //DECLARAMOS EL USESTATE PARA GUARDAR LOS OBJETOS QUE ME TRAE LA RESPUESTA
+    const [productos, setProductos]=useState([]);
+    useState
+    const API = import.meta.env.VITE_API;
+    const getProductos=async()=>{
+        try {
+            const response =await fetch(`${API}/productos`);//obtenemos la respuesta (el objeto)
+            //console.log("RESPONSE: ", response);
+            const resJson=await response.json();//Lo transformamos en un objeto JSON
+            //console.log("RESP JSON: ", resJson);
+            //guardamos el array que traemos de la base de datos en el useState
+            setProductos(resJson);
+        } catch (error) {
+            console.log("ERROR-->", error);
+        }
+    } 
+    //AHORA LLAMAREMOS A LA FUNCION GET PRODUCTOS CON LOS METODOS Y EFECTOS USEEFFECT
+
+    useEffect(()=>{
+        getProductos();
+        return()=>{//CON ESE RETURN HACEMOS EL DESMONTAJE PARA QUE NO CONSUMA MEMORIA RAM
+            setProductos([]);
+        }
+    },[])//el array vacio significa que la funcion se ejecutara solamente en el montaje
+
+    //console.log("State productos--> ",productos);
+  return (
+    <div className="container-fluid">
+        <div className="text-center mb-3">
+        <h2>Listado de Productos</h2>
         </div>
-    );
+      
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Titulo</th>
+            <th>Descripcion</th>
+            <th>Categoria</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {productos.map((element, index)=>{
+                return(
+                    <Producto producto={element} key={index}/>//AQUI LE PASAMOS LA PROP AL COMPONENTE PRODUCTO
+                )
+          })}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default ListadoProductos;
